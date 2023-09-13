3da578eab6b4495689722f8a9dca25d5398c9300
Austin Farmer <5827236+farmerau@users.noreply.github.com>
2023-09-13 19:49:48 -0400

    Off the cuff example

---
+++ Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonConsole
+{
+    internal class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set;  }
+        public int Weight { get; set; }
+        public IList<PokemonType> Types { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace PokemonConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+
+            while (true)
+            {
+                Console.WriteLine("What pokemon would you like to know about? Type 0 to exit.");
+                string pokemonChoice = Console.ReadLine().ToLower();
+                if (pokemonChoice == "0")
+                {
+                    return;
+                }
+
+                HttpResponseMessage responseMessage = await client.GetAsync($"pokemon/{pokemonChoice}");
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    Stream responseStream = await responseMessage.Content.ReadAsStreamAsync();
+                    Pokemon resultantPokemon = await JsonSerializer.DeserializeAsync<Pokemon>(responseStream, new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    });
+
+                    Console.WriteLine(JsonSerializer.Serialize(resultantPokemon));
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, we couldn't find that Pokemon.");
+                }
+
+            }
+        }
+    }
+}
